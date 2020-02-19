b38e3028f4a8cc5ec29f9392da714d15005999df
Anne de Joly <anne.dejoly@edhec.com>
2020-02-19 10:50:24 +0000

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  content = Faker::TvShows::BojackHorseman.quote
+  title = Faker::Book.title
+  Article.create(title: title, content: content)
+end
